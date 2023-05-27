a2a4b39b665ceea64527e92172087c6366a70899
bryanbocio <alexanderbocio@gmail.com>
2023-05-27 12:49:21 -0400

    agregando interfaz generica para los repositorios

---
+++ Core/Interfaces/GenericRepository/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces.GenericRepository
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+
+    }
+}
