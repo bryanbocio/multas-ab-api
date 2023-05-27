6baf1181ec2e95385d5289dea5d9b7d98397a090
bryanbocio <alexanderbocio@gmail.com>
2023-05-27 12:37:15 -0400

    agregando entidad TrafficFine que es la entidad de las multas

---
+++ Core/Entities/TrafficFine/TrafficFine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.TrafficFine
+{
+    public class TrafficFine : BaseEntity
+    {
+        public string IdDriver { get; set; }
+        public string CarPlate { get; set; }
+        public string reason { get; set; }
+        public string comment { get; set; }
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+
+        public DateTime DateCreated { get; }= DateTime.Now;
+
+    }
+}
