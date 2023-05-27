dd94afab0703f0bf507c057f631954304ff8090f
bryanbocio <alexanderbocio@gmail.com>
2023-05-27 13:35:24 -0400

    agregando configuracion de la tabla TrafficFine

---
+++ Infrastructure/Data/Config/TrafficFineConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities.TrafficFine;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Config
+{
+    internal class TrafficFineConfiguration : IEntityTypeConfiguration<TrafficFine>
+    {
+        public void Configure(EntityTypeBuilder<TrafficFine> builder)
+        {
+            builder.Property(trafficFine => trafficFine.Id).IsRequired();
+            builder.Property(trafficFine => trafficFine.CarPlate).IsRequired();
+            builder.Property(trafficFine => trafficFine.Reason).IsRequired().HasMaxLength(200);
+            builder.Property(trafficFine => trafficFine.Latitude).IsRequired();
+            builder.Property(trafficFine => trafficFine.Longitude).IsRequired();
+
+            builder.HasOne(trafficFine => trafficFine.Driver).WithMany().HasForeignKey(Fine => Fine.DriverId);
+            builder.HasOne(trafficFine => trafficFine.Agent).WithMany().HasForeignKey(Fine => Fine.AgentId);
+        }
+    }
+}
