1ca3face5427b87c642d518043aa524f6f1929bb
bryanbocio <alexanderbocio@gmail.com>
2023-05-28 16:52:02 -0400

    creando Clase GenericRepository e implementando interfaz

---
+++ Infrastructure/Data/Repositories/GenericRepository/GenericRepository.cs
@@ -0,0 +1,27 @@
+﻿using Core.Entities;
+using Core.Interfaces.GenericRepository;
+using Infrastructure.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.GenericRepository.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+    {
+
+        private readonly TrafficDbContext _dbContext;
+
+        public GenericRepository(TrafficDbContext dbContext)
+        {
+            _dbContext= dbContext;
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+    }
+}
