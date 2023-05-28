30562802842b70cf0ce107090db5f2eba9362661
bryanbocio <alexanderbocio@gmail.com>
2023-05-28 17:15:12 -0400

    Implementando metodo listAll

--- Infrastructure/Data/Repositories/GenericRepository/GenericRepository.cs
+++ Infrastructure/Data/Repositories/GenericRepository/GenericRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Core.Entities;
 using Core.Interfaces.GenericRepository;
 using Infrastructure.Data.Context;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,11 @@ namespace Infrastructure.Data.GenericRepository.Repositories
         public GenericRepository(TrafficDbContext dbContext)
         {
             _dbContext= dbContext;
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
         }
 
         public async Task<T> GetByIdAsync(int id)
