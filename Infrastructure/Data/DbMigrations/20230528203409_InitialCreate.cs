7920fb2ad034d93ce68cd88bca11715594621154
bryanbocio <alexanderbocio@gmail.com>
2023-05-28 16:45:24 -0400

    agregando migracion inicial

---
+++ Infrastructure/Data/DbMigrations/20230528203409_InitialCreate.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.DbMigration
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Agent",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AgentId = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    Number = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agent", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Driver",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DriverId = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    Number = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Driver", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TrafficFine",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    DriverId = table.Column<int>(type: "INTEGER", nullable: false),
+                    AgentId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CarPlate = table.Column<string>(type: "TEXT", nullable: false),
+                    Reason = table.Column<string>(type: "TEXT", maxLength: 200, nullable: false),
+                    Comment = table.Column<string>(type: "TEXT", nullable: false),
+                    Latitude = table.Column<string>(type: "TEXT", nullable: false),
+                    Longitude = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TrafficFine", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TrafficFine_Agent_AgentId",
+                        column: x => x.AgentId,
+                        principalTable: "Agent",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_TrafficFine_Driver_DriverId",
+                        column: x => x.DriverId,
+                        principalTable: "Driver",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrafficFine_AgentId",
+                table: "TrafficFine",
+                column: "AgentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TrafficFine_DriverId",
+                table: "TrafficFine",
+                column: "DriverId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TrafficFine");
+
+            migrationBuilder.DropTable(
+                name: "Agent");
+
+            migrationBuilder.DropTable(
+                name: "Driver");
+        }
+    }
+}
