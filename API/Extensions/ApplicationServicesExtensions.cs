72f8d1940c816f93d8d07077c2b471fdb50c7a71
bryanbocio <alexanderbocio@gmail.com>
2023-05-28 17:15:42 -0400

    registrando servicio GenericRepository

--- API/Extensions/ApplicationServicesExtensions.cs
+++ API/Extensions/ApplicationServicesExtensions.cs
@@ -1,12 +1,15 @@
-﻿namespace API.Extensions
+﻿using Core.Interfaces.GenericRepository;
+using Infrastructure.Data.GenericRepository.Repositories;
+
+namespace API.Extensions
 {
     public static class ApplicationServicesExtensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuracion)
         {
 
+            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
 
-           
 
             return services;
         }
