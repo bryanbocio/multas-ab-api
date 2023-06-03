a92c9d0b2dd748eb296aba4626285cb0ccce1345
bryanbocio <alexanderbocio@gmail.com>
2023-06-03 17:31:54 -0400

    agregando Metodo que trae todas las multas, pero implementando automapper, DTO y paginacion en el mismo, en este punto la API esta estable

--- API/Controllers/TrafficFines/TrafficFineController.cs
+++ API/Controllers/TrafficFines/TrafficFineController.cs
@@ -1,7 +1,11 @@
-﻿using API.Errors;
+﻿using API.DTOs;
+using API.Errors;
+using API.Helpers;
+using AutoMapper;
 using Core.Entities.TrafficFine;
 using Core.Interfaces.GenericRepository;
-using Microsoft.AspNetCore.Http;
+using Core.Specification;
+using Core.Specification.Parameters.TrafficFine;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers.TrafficFines
@@ -10,20 +14,29 @@ namespace API.Controllers.TrafficFines
     public class TrafficFineController : BaseController
     {
         private readonly IGenericRepository<TrafficFine> _fineRepository;
+        private readonly IMapper _mapper;
 
-        public TrafficFineController(IGenericRepository<TrafficFine> fineRepository)
+        public TrafficFineController(IGenericRepository<TrafficFine> fineRepository, IMapper mapper)
         {
             _fineRepository = fineRepository;
+            _mapper = mapper;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<Pagination<TrafficFineDto>>> GetTrafficFines([FromQuery] TrafficFineSpecificationParameters parameters)
+        {
+            var specifications = new TrafficFineWithDriverAndAgentSpecification(parameters);
+            var countSpecification= new TrafficFineWithFiltersForCountingSpecification(parameters);
+            var totalItems = await _fineRepository.CountAsync(countSpecification);
+            var trafficFines= await _fineRepository.ListAsync(specifications);
+
+            var data= _mapper.Map<IReadOnlyList<TrafficFine>, IReadOnlyList<TrafficFineDto>>(trafficFines);
+
+            return Ok(new Pagination<TrafficFineDto>(parameters.PageIndex, parameters.PageSize, totalItems, data));
         }
 
         
-        [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<TrafficFine>>> GetAllTrafficFines()
-        {
-            return Ok(await _fineRepository.GetAllAsync());
-        }
-
-
         [HttpGet("{id}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
