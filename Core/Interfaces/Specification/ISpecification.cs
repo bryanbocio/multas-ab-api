76555819c3e45b7586a732165755377b01521e23
bryanbocio <alexanderbocio@gmail.com>
2023-06-03 15:04:15 -0400

    agregando specification pattern

---
+++ Core/Interfaces/Specification/ISpecification.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface ISpecification<T>
+    {
+        Expression<Func<T, bool>> criteria { get; }
+
+        List<Expression<Func<T, object>>> includes { get; }
+
+        Expression<Func<T, object>> OrderBy { get; }
+        Expression<Func<T, object>> OrderByDescending { get; }
+
+        int Take { get; }
+
+        int Skip { get; }
+
+        bool IsPagingEnabled { get; }
+    }
+}
---
+++ Core/Specification/TrafficFineWithDriverAndAgentSpecification.cs
@@ -0,0 +1,38 @@
+﻿using Core.Entities.TrafficFine;
+using Core.Specification.Parameters.TrafficFine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specification
+{
+    public class TrafficFineWithDriverAndAgentSpecification : BaseSpecification<TrafficFine>
+    {
+
+        public TrafficFineWithDriverAndAgentSpecification(TrafficFineSpecificationParameters parameters) 
+            : base(fine=>
+
+                (string.IsNullOrEmpty(parameters.Search) || fine.CarPlate.ToLower().Contains(parameters.Search))
+                 &&
+                (!parameters.DriverId.HasValue || fine.DriverId == parameters.DriverId)
+                &&
+                (!parameters.AgentId.HasValue || fine.AgentId == parameters.AgentId)
+                )
+
+        {
+
+            AddInclude(fine => fine.Driver);
+            AddInclude(fine => fine.Agent);
+            AddOrderBy(fine => fine.DateCreated);
+
+        }
+
+        public TrafficFineWithDriverAndAgentSpecification(int id) : base(fine => fine.Id == id)
+        {
+            AddInclude(fine => fine.Driver);
+            AddInclude(fine=> fine.Agent);
+        }
+    }
+}
