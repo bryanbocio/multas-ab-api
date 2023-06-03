44f9e89a998f3705ec9ef0148a3502042cb8bf56
bryanbocio <alexanderbocio@gmail.com>
2023-06-03 17:29:00 -0400

    agregando los metodos con specification que faltaba a la interfaz generica de los repositorios

--- Core/Interfaces/GenericRepository/IGenericRepository.cs
+++ Core/Interfaces/GenericRepository/IGenericRepository.cs
@@ -12,5 +12,17 @@ namespace Core.Interfaces.GenericRepository
         Task<T> GetByIdAsync(int id);
 
         Task<IReadOnlyList<T>> GetAllAsync();
+
+        Task<T> GetEntityWithSpecification(ISpecification<T> specification);
+
+        Task<IReadOnlyList<T>> ListAsync(ISpecification<T> specification);
+
+        Task<int> CountAsync(ISpecification<T> specification);
+
+        void Add(T entity);
+
+        void Update(T entity);
+
+        void Delete(T entity);
     }
 }
