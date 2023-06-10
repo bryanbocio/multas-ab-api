f535595ac8d1c56793250fdddf1db099bde02706
bryanbocio <alexanderbocio@gmail.com>
2023-06-10 18:12:06 -0400

    agregando Parametros para el Agent

---
+++ Core/Specification/Parameters/Driver/AgentSpecificationParameter.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Core.Specification.Parameters.Driver
+{
+    public class AgentSpecificationParameter
+    {
+        private const int MaxPageSize = 50;
+        public int PageIndex { get; set; } = 1;
+
+        private int _pagSize = 6;
+
+        public int PageSize
+        {
+            get => _pagSize;
+            set => _pagSize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+
+        public string? AgentId { get; set; }
+        public string? Sort { get; set; }
+
+        private string _search;
+
+        public string? Search
+        {
+            get => _search;
+            set => _search = value.ToLower();
+        }
+    }
+}
