a4628ed9237e91e9f96271f34c2928f6c29a4671
bryanbocio <alexanderbocio@gmail.com>
2023-06-10 17:46:23 -0400

    agregando dtos de agente

---
+++ API/DTOs/AgentDto.cs
@@ -0,0 +1,10 @@
+﻿namespace API.DTOs
+{
+    public class AgentDto
+    {
+        public string AgentIdentity { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
---
+++ API/DTOs/AgentToReturn.cs
@@ -0,0 +1,11 @@
+﻿namespace API.DTOs
+{
+    public class AgentToReturn
+    {
+        public int Id { get; set; }
+        public string AgentIdentity { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
