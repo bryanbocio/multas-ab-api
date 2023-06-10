4371cf6a9ef32f8ddb958397e3325cb57481b7f1
bryanbocio <alexanderbocio@gmail.com>
2023-06-10 15:56:20 -0400

    Cambiando el nombre del TrafficFineDto

---
+++ API/DTOs/TrafficFineToReturnDto.cs
@@ -0,0 +1,18 @@
+﻿namespace API.DTOs
+{
+    public class TrafficFineToReturnDto
+    {
+        public int Id { get; set; }
+        public string DriverIdentity { get; set; }
+        public string DriverName { get; set; }
+        public string DriverPhoneNumber { get; set; }
+        public string CarPlate { get; set; }
+        public string Reason { get; set; }
+        public string Comment { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string AgentIdentity { get; set; }
+
+    }
+}
