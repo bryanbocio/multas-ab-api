c04bea78c82c6a097c1dc8e406ec98662d0af40f
bryanbocio <alexanderbocio@gmail.com>
2023-06-10 17:27:49 -0400

    agregando servicio que crea una TrafficFine (Multa)

--- Infrastructure/Data/Services/TrafficFineService.cs
+++ Infrastructure/Data/Services/TrafficFineService.cs
@@ -1,6 +1,9 @@
-﻿using Core.Entities.TrafficFine;
+﻿using Core.Entities.Agent;
+using Core.Entities.Driver;
+using Core.Entities.TrafficFine;
 using Core.Interfaces.Services;
 using Core.Interfaces.UnitOfWork;
+using Core.Specification;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,9 +21,34 @@ namespace Infrastructure.Data.Services
             _unitOfWork = unitOfWork;
         }
 
-        public Task<TrafficFine> CreateTrafficFine(string driverIdentity, string agentIdentity, string carPlate, string Reason, string comment, string latitude, string longitude, DateTime dateCreate)
+        public async Task<TrafficFine> CreateTrafficFine(string driverIdentity, string agentIdentity, string carPlate, string reason, string comment, string latitude, string longitude, DateTime dateCreate)
         {
-            throw new NotImplementedException();
+            var driver = await  _unitOfWork.Repository<Driver>().GetEntityWithSpecification(new DriverSpecification(driverIdentity));
+          
+            var agent  = await  _unitOfWork.Repository<Agent>().GetEntityWithSpecification(new AgentSpecification(agentIdentity));
+
+            var trafficFine = new TrafficFine
+                (
+                   driver,
+                   agent,
+                   carPlate,
+                   reason,
+                   comment,
+                   latitude,
+                   longitude
+                );
+
+            _unitOfWork.Repository<TrafficFine>().Add(trafficFine);
+
+            var result = await _unitOfWork.Complete();
+
+            if (result <= 0) return null;
+
+
+            return trafficFine;
+
+
+
         }
     }
 }
