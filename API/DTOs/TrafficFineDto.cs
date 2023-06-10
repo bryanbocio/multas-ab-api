8b94a12606a2757d1061d3b67abca7e8dff67862
bryanbocio <alexanderbocio@gmail.com>
2023-06-10 16:38:31 -0400

    Agregando TrafficFineDto

---
+++ API/DTOs/TrafficFineDto.cs
@@ -0,0 +1,16 @@
+﻿namespace API.DTOs
+{
+    public class TrafficFineDto
+    {
+        public string DriverIdentity { get; set; }
+        public string DriverName { get; set; }
+        public string DriverPhoneNumber { get; set; }
+        public string CarPlate { get; set; }
+        public string Reason { get; set; }
+        public string Comment { get; set; }
+        public string Latitude { get; set; }
+        public string Longitude { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string AgentIdentity { get; set; }
+    }
+}
