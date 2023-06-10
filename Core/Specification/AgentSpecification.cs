7fb5ad5fc96fbc41af8d6aaeab2dfa7a2143b48c
bryanbocio <alexanderbocio@gmail.com>
2023-06-10 17:27:18 -0400

    creando agentSpecification

---
+++ Core/Specification/AgentSpecification.cs
@@ -0,0 +1,18 @@
+﻿using Core.Entities.Agent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specification
+{
+    public class AgentSpecification : BaseSpecification<Agent>
+    {
+
+        public AgentSpecification(string agentIdentity): base(agent=> agent.AgentId==agentIdentity)
+        {
+
+        }
+    }
+}
