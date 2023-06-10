d8f6ccc375fefd1845c94f1c969a282318a2a4f0
bryanbocio <alexanderbocio@gmail.com>
2023-06-10 18:17:34 -0400

    agregando nuevo contructor para DriverSpecification

--- Core/Specification/DriverSpecification.cs
+++ Core/Specification/DriverSpecification.cs
@@ -1,11 +1,23 @@
 ﻿
 using Core.Entities.Driver;
+using Core.Specification.Parameters.Driver;
 
 namespace Core.Specification
 {
     public class DriverSpecification : BaseSpecification<Driver>
     {
         public DriverSpecification(string driverIdentity) : base(driver=> driver.DriverId==driverIdentity)
+        {
+
+        }
+
+        public DriverSpecification(DriverSpecificationParameters parameters) 
+            : base(driver =>
+
+                 (string.IsNullOrEmpty(parameters.Search) || driver.Name.ToLower().Contains(parameters.Search))
+                  &&
+                 (string.IsNullOrEmpty(parameters.DriverId) || driver.DriverId == parameters.DriverId)
+                  )
         {
 
         }
