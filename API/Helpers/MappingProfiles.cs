f6cd64b07df01a6d3ca3557a28bf4ed8de2d6232
bryanbocio <alexanderbocio@gmail.com>
2023-06-10 16:16:22 -0400

    agregando los mapping profile de los Dto de driver

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -1,5 +1,6 @@
 ﻿using API.DTOs;
 using AutoMapper;
+using Core.Entities.Driver;
 using Core.Entities.TrafficFine;
 
 namespace API.Helpers
@@ -13,6 +14,15 @@ namespace API.Helpers
                             .ForMember(fineDto=> fineDto.DriverIdentity, options=> options.MapFrom(fine=> fine.Driver.DriverId))
                             .ForMember(fineDto=> fineDto.DriverPhoneNumber, options=> options.MapFrom(fine=> fine.Driver.Number))
                             .ForMember(fineDto=> fineDto.AgentIdentity, options=> options.MapFrom(fine=> fine.Agent.Name));
+
+
+            CreateMap<Driver, DriverToReturnDto>()
+                                .ForMember(driverDto=> driverDto.DriverIdentity,option=> option.MapFrom(driver=>driver.DriverId))
+                                .ForMember(driverDto => driverDto.PhoneNumber, option => option.MapFrom(driver => driver.Number))
+                                .ReverseMap();
+            CreateMap<DriverDto, Driver>()
+                                .ForMember(driver=> driver.DriverId, options=>options.MapFrom(driverDto=>driverDto.DriverIdentity))
+                                .ForMember(driver => driver.Number, option => option.MapFrom(driverDto => driverDto.PhoneNumber));
         }
     }
 }
