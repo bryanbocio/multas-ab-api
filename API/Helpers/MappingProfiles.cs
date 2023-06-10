f8a2c8d12bafce652526a8134b77f48497f33976
bryanbocio <alexanderbocio@gmail.com>
2023-06-10 17:47:08 -0400

    agregando los mapping profile de Agente

--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -1,5 +1,6 @@
 ﻿using API.DTOs;
 using AutoMapper;
+using Core.Entities.Agent;
 using Core.Entities.Driver;
 using Core.Entities.TrafficFine;
 
@@ -26,9 +27,19 @@ namespace API.Helpers
                                 .ForMember(driverDto=> driverDto.DriverIdentity,option=> option.MapFrom(driver=>driver.DriverId))
                                 .ForMember(driverDto => driverDto.PhoneNumber, option => option.MapFrom(driver => driver.Number))
                                 .ReverseMap();
+
             CreateMap<DriverDto, Driver>()
                                 .ForMember(driver=> driver.DriverId, options=>options.MapFrom(driverDto=>driverDto.DriverIdentity))
                                 .ForMember(driver => driver.Number, option => option.MapFrom(driverDto => driverDto.PhoneNumber));
+
+            CreateMap<Agent, AgentToReturn>()
+                                .ForMember(agentDto => agentDto.AgentIdentity, option => option.MapFrom(driver => driver.AgentId))
+                                .ForMember(agentDto => agentDto.PhoneNumber, option => option.MapFrom(driver => driver.Number))
+                                .ReverseMap();
+
+            CreateMap<AgentDto, Agent>()
+                               .ForMember(agent => agent.AgentId, options => options.MapFrom(driverDto => driverDto.AgentIdentity))
+                               .ForMember(agent => agent.Number, option => option.MapFrom(driverDto => driverDto.PhoneNumber));
         }
     }
 }
