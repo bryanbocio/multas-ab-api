9275587737b4a0385ba60a37211ca7c8d6ac824e
bryanbocio <alexanderbocio@gmail.com>
2023-06-10 17:47:37 -0400

    mejorando el controller de Agente

--- API/Controllers/Agents/AgentController.cs
+++ API/Controllers/Agents/AgentController.cs
@@ -1,6 +1,9 @@
-﻿using API.Errors;
+﻿using API.DTOs;
+using API.Errors;
+using AutoMapper;
 using Core.Entities.Agent;
 using Core.Interfaces.UnitOfWork;
+using Core.Specification;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers.Agents
@@ -9,24 +12,40 @@ namespace API.Controllers.Agents
     {
 
         private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
 
-        public AgentController(IUnitOfWork unitOfWork)
+        public AgentController(IUnitOfWork unitOfWork , IMapper mapper)
         {
             _unitOfWork = unitOfWork;
+            _mapper = mapper;
+            
         }
 
-
-
         [HttpPost]
-        public async Task<ActionResult> CreateAgent(Agent agent)
+        public async Task<ActionResult> CreateAgent(AgentDto agent)
         {
-            _unitOfWork.Repository<Agent>().Add(agent);
+            var agentForRegistering= _mapper.Map<AgentDto, Agent>(agent);
+
+            _unitOfWork.Repository<Agent>().Add(agentForRegistering);
 
             var result =await _unitOfWork.Complete();
 
             if (result <= 0) return BadRequest(new ApiResponse(400, "Problems creating a Agent"));
 
-            return Ok();
+            return Ok(new ApiResponse(201));
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AgentToReturn>> GetDriver(string id)
+        {
+            var specification = new AgentSpecification(id);
+
+            var agent = await _unitOfWork.Repository<Agent>().GetEntityWithSpecification(specification);
+
+            if (agent == null) return NotFound(new ApiResponse(404));
+
+            return Ok(_mapper.Map<Agent, AgentToReturn>(agent));
         }
 
     }
