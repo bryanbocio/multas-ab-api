1647ca928d5ddd0152887fbd10be306868c51a78
bryanbocio <alexanderbocio@gmail.com>
2023-06-10 16:39:20 -0400

    implementando interfaz ITrafficFineServices

---
+++ Infrastructure/Data/Services/TrafficFineService.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities.TrafficFine;
+using Core.Interfaces.Services;
+using Core.Interfaces.UnitOfWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Services
+{
+    public class TrafficFineService : ITrafficFineService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public TrafficFineService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public Task<TrafficFine> CreateTrafficFine(string driverIdentity, string agentIdentity, string carPlate, string Reason, string comment, string latitude, string longitude, DateTime dateCreate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
