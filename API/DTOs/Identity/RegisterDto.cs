87aa9cd151a52384b8f930fc00c94a4e19dfcfd0
bryanbocio <alexanderbocio@gmail.com>
2023-06-17 16:41:14 -0400

    Agregando dtos de user, register y login

---
+++ API/DTOs/Identity/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs.Identity
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string IdentityUserId { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
+            ErrorMessage = "Password must have 1 Uppercase, 1 Lowercase, 1 number, 1 non alphanumeric and at least 6 characters")]
+        public string Password { get; set; }
+    }
+}
---
+++ API/DTOs/Identity/UserDto.cs
@@ -0,0 +1,9 @@
+﻿namespace API.DTOs.Identity
+{
+    public class UserDto
+    {
+        public string Email { get; set; }
+        public string IdentityUserId { get; set; }
+        public string Token { get; set; }
+    }
+}
