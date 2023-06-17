08b2bcb5cff8871f49e7ceb38c2e34544d6154f4
bryanbocio <alexanderbocio@gmail.com>
2023-06-17 15:29:34 -0400

    agregando extension de identity para asi tener todo los servicios en una clase separada totalmente del negocio

---
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -0,0 +1,38 @@
+﻿using Infrastructure.Data.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace API.Extensions
+{
+    public static class IdentityServiceExtensions
+    {
+        public static IServiceCollection AddIdentityServices(this IServiceCollection service, IConfiguration config)
+        {
+            service.AddDbContext<AppIdentityDbContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("IdentityConnection"));
+            });
+
+            service.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt =>
+            {
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Token:Key"])),
+                    ValidIssuer = config["Token:Issuer"],
+                    ValidAudience = config["Token:Audience"],
+                    ValidateLifetime = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = false,
+                };
+            });
+
+            service.AddAuthorization();
+
+
+            return service;
+        }
+    }
+}
