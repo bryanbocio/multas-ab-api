42024cb46ca384bfec446f3094370b15daa9dbc7
bryanbocio <alexanderbocio@gmail.com>
2023-06-18 16:27:38 -0400

    agregando clases utilitarias para Driver y Agent

---
+++ Infrastructure/Data/Utils/UtilDriver.cs
@@ -0,0 +1,28 @@
+﻿using Core.Entities.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Utils
+{
+    public class UtilDriver
+    {
+
+
+        public static Driver BuildDriverObject(string driverIdentity,string name, string lastName, string phoneNumber)
+        {
+
+
+
+            return new Driver 
+            {
+                DriverId= driverIdentity,
+                Name= name,
+                LastName= lastName,
+                Number= phoneNumber,
+            };
+        }
+    }
+}
---
+++ Infrastructure/Data/Utils/UtilsAgent.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities.Agent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Utils
+{
+    public class UtilsAgent
+    {
+        public static Agent BuildAgentObject(string agentIdentity, string name, string lastName, string phoneNumber)
+        {
+            return new Agent
+            {
+                AgentId = agentIdentity,
+                Name = name,
+                LastName = lastName,
+                Number = phoneNumber,
+            };
+        }
+    }
+}
