a40fa7b53f983ee670586c80d3f1a6b114c0856c
bryanbocio <alexanderbocio@gmail.com>
2023-06-18 14:49:29 -0400

    agregando interfza e implementacion del servicio para usuarios, para no repetir tanto el codigo de Identity

---
+++ Core/Interfaces/Services/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces.Services
+{
+    public interface IUserService
+    {
+        Task<bool> CreateUserAsync(AppUser user, string password, string rol);
+    }
+}
---
+++ Infrastructure/Data/Services/UserService.cs
@@ -0,0 +1,60 @@
+﻿using Core.Entities.Identity;
+using Core.Enums;
+using Core.Interfaces.Services;
+using Core.Interfaces.UnitOfWork;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Services
+{
+    public class UserService : IUserService
+    {
+
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IUnitOfWork _unitOfWork;
+        public UserService(UserManager<AppUser> userManager, IUnitOfWork unitOfWork)
+        {
+            _userManager = userManager;
+            _unitOfWork = unitOfWork;
+        }
+        public async  Task<bool> CreateUserAsync(AppUser user, string password, string rol)
+        {
+
+
+            var userCreated = await _userManager.CreateAsync(user, password);
+
+            var rolesAvalibles = Enum.GetValues(typeof(Roles));
+
+            await AsignRoles(rolesAvalibles,rol, user); 
+
+            return userCreated.Succeeded;
+
+        }
+
+
+        private async Task AsignRoles(Array rolesAvalibles, string rolToAsing, AppUser appUser)
+        {
+
+            if (String.IsNullOrEmpty(rolToAsing))
+            {
+                await _userManager.AddToRoleAsync(appUser, Roles.USER.ToString());
+            }
+            else
+            {
+                foreach (var role in rolesAvalibles)
+                {
+                    if (role.Equals(rolToAsing))
+                    {
+                        await _userManager.AddToRoleAsync(appUser, role.ToString());
+                    }
+                }
+            }
+
+           
+        }
+    }
+}
