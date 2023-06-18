2b63f88ddd806dcbed4a1fba41014ae8e29e2619
bryanbocio <alexanderbocio@gmail.com>
2023-06-18 16:27:54 -0400

    agregando fields faltantes a RegisterDto

--- API/DTOs/Identity/RegisterDto.cs
+++ API/DTOs/Identity/RegisterDto.cs
@@ -15,6 +15,9 @@ namespace API.DTOs.Identity
         [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
             ErrorMessage = "Password must have 1 Uppercase, 1 Lowercase, 1 number, 1 non alphanumeric and at least 6 characters")]
         public string Password { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
         public string? Role { get; set; }
     }
 }
