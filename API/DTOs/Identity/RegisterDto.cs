a334da33744eefe66c3b83dc2b254150400ac7ee
bryanbocio <alexanderbocio@gmail.com>
2023-06-18 16:51:01 -0400

    eliminando Rol de RegisterDto y creando RegisterForAdminDto para que los administradores 100% tengan las capacidad de tener poder para asignar roles de user, admin o agent

--- API/DTOs/Identity/RegisterDto.cs
+++ API/DTOs/Identity/RegisterDto.cs
@@ -18,6 +18,6 @@ namespace API.DTOs.Identity
         public string Name { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
-        public string? Role { get; set; }
+      
     }
 }
