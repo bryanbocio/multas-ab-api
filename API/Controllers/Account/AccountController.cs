97af34b97ca1d50ae243c9cf2796fc7eff518f09
bryanbocio <alexanderbocio@gmail.com>
2023-06-18 14:51:21 -0400

    agregando endpoint de register rolo para administradores

--- API/Controllers/Account/AccountController.cs
+++ API/Controllers/Account/AccountController.cs
@@ -3,7 +3,9 @@ using API.Errors;
 using API.Extensions;
 using AutoMapper;
 using Core.Entities.Identity;
+using Core.Interfaces.Services;
 using Core.Interfaces.Tokens;
+using Core.Interfaces.UnitOfWork;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -15,15 +17,23 @@ namespace API.Controllers.Account
         private readonly UserManager<AppUser> _userManager;
         private readonly SignInManager<AppUser> _signInManager;
         private readonly ITokenService _tokeService;
+        private readonly IUserService _userService;
+        private readonly IUnitOfWork _unitOfWork;
+       
 
 
         public AccountController(UserManager<AppUser> userManager,
                                  SignInManager<AppUser> signInManager,
-                                 ITokenService tokenService)
+                                 ITokenService tokenService,
+                                 IUserService userService,
+                                 IUnitOfWork unitOfWork)
+                                 
         {
+            _unitOfWork= unitOfWork;
             _userManager = userManager;
             _signInManager = signInManager;
             _tokeService = tokenService;
+            _userService = userService;
         }
 
         [Authorize]
@@ -83,9 +93,10 @@ namespace API.Controllers.Account
                 UserName = registerDto.Email
             };
 
-            var result = await _userManager.CreateAsync(user, registerDto.Password);
 
-            if (!result.Succeeded) return BadRequest(new ApiResponse(400));
+            var result = await _userService.CreateUserAsync(user,registerDto.Password,registerDto.Role);
+
+            if (!result) return BadRequest(new ApiResponse(400));
 
             return new UserDto
             {
@@ -95,7 +106,33 @@ namespace API.Controllers.Account
             };
         }
 
+        [Authorize(Roles="ADMIN")]
+        [HttpPost("registerbyadmin")]
+        public async Task<ActionResult<UserDto>> RegisterUserByAdmin(RegisterDto registerDto)
+        {
+            if (CheckEmailExistAsync(registerDto.Email).Result.Value)
+            {
+                return new BadRequestObjectResult(new ApiValidationErrorResponse { Errors = new[] { "Email address is in use" } });
+            }
+
+            var user = new AppUser
+            {
+                IdentityAppUser = registerDto.IdentityUserId,
+                Email = registerDto.Email,
+                UserName = registerDto.Email
+            };
 
 
+            var result = await _userService.CreateUserAsync(user, registerDto.Password, registerDto.Role);
+
+            if (!result) return BadRequest(new ApiResponse(400));
+
+            return new UserDto
+            {
+                Email = user.Email,
+                IdentityUserId = user.IdentityAppUser,
+                Token = await _tokeService.CreateToken(user),
+            };
+        }
     }
 }
