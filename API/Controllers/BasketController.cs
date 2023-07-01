b95a6bac214cb62e94aae218f51fc04f086a7fa6
bryanbocio <alexanderbocio@gmail.com>
2023-07-01 15:04:32 -0400

    agregando controller para el carrito o Basket

---
+++ API/Controllers/BasketController.cs
@@ -0,0 +1,43 @@
+﻿using API.DTOs;
+using AutoMapper;
+using Core.Entities.Basket;
+using Core.Interfaces.Reporitories.BasketRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class BasketController : BaseController
+    {
+            private readonly IBasketRepository _basketRepository;
+            private readonly IMapper _mapper;
+
+            public BasketController(IBasketRepository basketRepository, IMapper mapper)
+            {
+                _basketRepository = basketRepository;
+                _mapper = mapper;
+            }
+
+            [HttpGet]
+            public async Task<ActionResult<CustomerBasket>> GetBasketById(string id)
+            {
+                var basket = await _basketRepository.GetBasketAsync(id);
+                return Ok(basket ?? new CustomerBasket(id));
+            }
+
+            [HttpPost]
+            public async Task<ActionResult<CustomerBasket>> UpdateBasket(CustomerBasketDto basket)
+            {
+                var customerBasket = _mapper.Map<CustomerBasketDto, CustomerBasket>(basket);
+
+                var updatedBasket = await _basketRepository.UpdateBasketAsync(customerBasket);
+
+                return Ok(updatedBasket);
+            }
+
+            [HttpDelete]
+            public async Task DeleteBasketAsync(string id)
+            {
+                await _basketRepository.DeleteBasketAsync(id);
+            }
+        }
+}
