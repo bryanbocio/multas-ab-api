a5c8f7deeb8b30c0c9b8bcba72ddbff60bbf97a1
bryanbocio <alexanderbocio@gmail.com>
2023-07-01 15:04:00 -0400

    agregando dtos para la parte del carrito

---
+++ API/DTOs/BasketItemsDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class BasketItemsDto
+    {
+        [Required]
+
+        public int Id { get; set; }
+
+        [Required]
+        public int TrafficFineId { get; set; }
+
+        [Required]
+        [Range(0.10, double.MaxValue, ErrorMessage = "Price must be greater than zero")]
+        public decimal TrafficFinePrice { get; set; }
+    }
+}
