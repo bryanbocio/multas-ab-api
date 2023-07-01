60e7f4f9bce5f8b1f27a4545cf712cf3f242cefe
bryanbocio <alexanderbocio@gmail.com>
2023-07-01 14:49:20 -0400

    agregando la interfaz de basket repository

---
+++ Core/Interfaces/Reporitories/BasketRepository/IBasketRepository.cs
@@ -0,0 +1,12 @@
+﻿
+using Core.Entities.Basket;
+
+namespace Core.Interfaces.Reporitories.BasketRepository
+{
+    public interface IBasketRepository
+    {
+        Task<CustomerBasket> GetBasketAsync(string basketId);
+        Task<CustomerBasket> UpdateBasketAsync(CustomerBasket basket);
+        Task<bool> DeleteBasketAsync(string basketId);
+    }
+}
