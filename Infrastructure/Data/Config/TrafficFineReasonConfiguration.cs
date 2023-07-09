922499b91ec6c9f427b9f64ac2e7530437efcbc0
bryanbocio <alexanderbocio@gmail.com>
2023-07-09 14:36:55 -0400

    agregando configuracion para las nuevas entidades que se acaban de crear

---
+++ Infrastructure/Data/Config/TrafficFineReasonConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities.TrafficFine;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data.Config
+{
+    public class TrafficFineReasonConfiguration : IEntityTypeConfiguration<TrafficFineReason>
+    {
+        public void Configure(EntityTypeBuilder<TrafficFineReason> builder)
+        {
+            builder.Property(reason => reason.Price).HasColumnType("decimal(18,2)");
+
+        }
+    }
+}
