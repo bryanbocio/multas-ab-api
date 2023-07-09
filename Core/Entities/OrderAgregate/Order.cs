d75fba6a804cc08bcb75cbb74dfbd1ac3e1a3350
bryanbocio <alexanderbocio@gmail.com>
2023-07-09 14:35:52 -0400

    agregando todas las clases pertenecientes a una orden para poder pagar una multa

---
+++ Core/Entities/OrderAgregate/Order.cs
@@ -0,0 +1,29 @@
+﻿
+
+namespace Core.Entities.OrderAgregate
+{
+    public class Order : BaseEntity
+    {
+        public Order()
+        {
+
+        }
+
+        public Order(IReadOnlyList<OrderItem> orderItems,
+                     string driverId,
+                     decimal subTotal)
+        {
+            OrderItems = orderItems;
+            DriverId = driverId;
+            SubTotal= subTotal;
+        }
+
+        public string DriverId { get; set; }
+        public DateTime OrderDate { get; set; }= DateTime.UtcNow;
+        public IReadOnlyList<OrderItem> OrderItems { get; set; }
+        public decimal SubTotal { get; set; }
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+        public string? PaymentIntentId { get; set; }
+
+    }
+}
---
+++ Core/Entities/OrderAgregate/OrderStatus.cs
@@ -0,0 +1,16 @@
+﻿using System.Runtime.Serialization;
+
+namespace Core.Entities.OrderAgregate
+{
+    public enum OrderStatus
+    {
+        [EnumMember(Value = "Pending")]
+        Pending,
+
+        [EnumMember(Value = "Payment Received")]
+        PaymentRecevied,
+
+        [EnumMember(Value = "Payment Failed")]
+        PaymentFailed
+    }
+}
