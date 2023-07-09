66ef4a0aefa62f9088b6da802966c68541a5aef7
bryanbocio <alexanderbocio@gmail.com>
2023-07-09 14:36:07 -0400

    Haciendo que las Rasones de multa sean una entidad

---
+++ Core/Entities/TrafficFine/TrafficFineReason.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.TrafficFine
+{
+    public class TrafficFineReason
+    {
+        public int Id { get; set; }
+        public string Reason { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
