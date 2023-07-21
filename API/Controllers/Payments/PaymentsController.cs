4c3327411e4653122484e363509c50c56ab35aef
bryanbocio <alexanderbocio@gmail.com>
2023-07-21 08:26:34 -0400

    agregando paymentIntent

---
+++ API/Controllers/Payments/PaymentsController.cs
@@ -0,0 +1,26 @@
+﻿using Core.Entities.Basket;
+using Core.Interfaces.Payment;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers.Payments
+{
+    public class PaymentsController : BaseController
+    {
+        private readonly IPaymentService _paymentService;
+
+        public PaymentsController(IPaymentService paymentService)
+        {
+            _paymentService = paymentService;
+        }
+
+        [Authorize]
+        [HttpPost("{basketId}")]
+        public async Task<ActionResult<CustomerBasket>> CreateOrUpdatePaymentIntent(string basketId)
+        {
+            return await _paymentService.CreateOrUpdatePaymentIntent(basketId);
+        }
+
+        
+    }
+}
---
+++ Core/Interfaces/Payment/IPaymentService.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entities.Basket;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces.Payment
+{
+    public interface IPaymentService
+    {
+        Task<CustomerBasket> CreateOrUpdatePaymentIntent(string basketId);
+    }
+}
